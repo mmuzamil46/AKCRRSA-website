04d8052888150f1f79650ed685c30626faf6c1c9
mmuzamil46 <servhost22@gmail.com>
2025-12-17 15:55:05 +0300

    backto render

--- server/server.js
+++ server/server.js
@@ -56,10 +56,8 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
