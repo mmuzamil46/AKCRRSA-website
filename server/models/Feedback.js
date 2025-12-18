05d7c4a2190d16531e241ad82181afc7234dcea8
mmuzamil46 <servhost22@gmail.com>
2025-12-18 11:15:09 +0300

    feed back

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -25,7 +25,7 @@ const feedbackSchema = new mongoose.Schema({
     },
     phone: {
         type: String,
-        required: true // Making it required based on user request "most users use phone", but can be optional if needed. Let's make it required as user emphasized it.
+        required: false
     },
     status: {
         type: String,
